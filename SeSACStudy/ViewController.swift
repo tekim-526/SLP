990e4088efb060e6a067f3577641669394a8a012
tekim <rldod526@naver.com>
2022-11-07 12:12:02 +0900

    initial commit

---
+++ SeSACStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACStudy
+//
+//  Created by Kim TaeSoo on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
